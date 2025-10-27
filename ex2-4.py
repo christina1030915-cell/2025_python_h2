659a819dbdf62136779fbd3bea1e62a5852fb9df
christina1030915-cell <christina1030915@gmail.com>
2025-10-27 20:53:30 +0800

    Create ex2-4.py

---
+++ ex2-4.py
@@ -0,0 +1,15 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+# this is coment method 1
+print ('Hello World') # This is coment method 1
+# this is coment method 1
+
+A = "hello world from variable "
+
+print (A) 
