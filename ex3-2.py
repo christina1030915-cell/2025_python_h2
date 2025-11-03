5d3ff979e88c392999d899b302019e70ff2c336d
christina1030915-cell <christina1030915@gmail.com>
2025-11-03 19:30:15 +0800

    Create ex3-2.py
    Fix variable name and add missing variable definition.

---
+++ ex3-2.py
@@ -0,0 +1,19 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+# 建立變數 cl
+n2ame = "張三"
+age_old = 30
+class1 = 175.5
+is_student = True
+
+# 輸出變數
+print(n2ame)
+print(age_old)
+print(height)
+print(is_student)
