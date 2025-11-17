a9836d182d9d0b5984cf4edaf56f68a5bf7d9baf
christina1030915-cell <christina1030915@gmail.com>
2025-11-17 20:37:14 +0800

    Create ex 5-4.py

---
+++ ex 5-4.py
@@ -0,0 +1,28 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+成績
+
+'''
+print("我可以告訴你你正課的平均成績是否及格")
+
+cccc=float(input("你的國語成績是多少:"))
+mmmm=float(input("你的數學成績是多少:"))
+eeee=float(input("你的英文成績是多少:"))
+ssss=float(input("你的自然成績是多少:"))
+sscc=float(input("你的社會成績是多少:"))
+average=(cccc+mmmm+eeee+ssss+sscc)/5
+if (average>60):{
+    print(f"你的平均成績是{average}，及格  ٩(^ᴗ^)۶")
+}
+if (average<60):{
+    print(f"你的平均成績是{average}，不及格   ／︿＼ ")
+}
+if (average=60):{
+    print(f"你的平均成績是{average}，剛好及格    ＼`•̀益•  }")
+}
