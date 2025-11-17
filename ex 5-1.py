57827f6ae0e37818a4d3a6a94d5e3b6f1b0b21d5
christina1030915-cell <christina1030915@gmail.com>
2025-11-17 19:28:59 +0800

    Create ex 5-1.py

---
+++ ex 5-1.py
@@ -0,0 +1,12 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+print(f"是學生：{is_student}")
+print(f"已畢業：{is_graduated}")
+print(f"True 的型別：{type(True)}")
+print(f"False 的型別：{type(False)}")
