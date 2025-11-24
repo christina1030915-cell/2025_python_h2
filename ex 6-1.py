917c562c38bbec714bbb088ed1779b0da3de35a7
christina1030915-cell <christina1030915@gmail.com>
2025-11-24 20:25:13 +0800

    Create ex 6-1.py

---
+++ ex 6-1.py
@@ -0,0 +1,12 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+
+r = float(input("please input a radius = "))
+print("\n length = ", 2*3.14* r)
+print("\n area = ", 3.14 *r *r)
