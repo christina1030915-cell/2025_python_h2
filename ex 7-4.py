5e0d17f3254739f84e5208fbd6af9a0d576ed766
christina1030915-cell <christina1030915@gmail.com>
2025-12-01 20:26:04 +0800

    Create ex 7-4.py

---
+++ ex 7-4.py
@@ -0,0 +1,17 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+# 計算 1 到 100 的總和
+total = 0
+number = 1
+
+while number <= 100:
+    total += number
+    number += 1
+
+print(f"1 到 100 的總和是：{total}")
