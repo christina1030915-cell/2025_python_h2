6f9cc952896213f860e3af26eac5f2b79ce4f09a
christina1030915-cell <christina1030915@gmail.com>
2025-12-01 19:53:02 +0800

    Create ex 7-3.py

---
+++ ex 7-3.py
@@ -0,0 +1,15 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+count = 1
+
+while count <= 5:
+    print(f"數字：{count}")
+    count += 1  # 重要：更新計數器
+
+print("迴圈結束")
