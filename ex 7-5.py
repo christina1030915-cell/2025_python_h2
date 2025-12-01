3d853449d05e0b3de76c6e8ad5e1f7ea80e6ead3
christina1030915-cell <christina1030915@gmail.com>
2025-12-01 20:26:35 +0800

    Create ex 7-5.py

---
+++ ex 7-5.py
@@ -0,0 +1,29 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+while True:
+    print("\n=== 簡單計算機 ===")
+    print("1. 加法")
+    print("2. 減法")
+    print("3. 退出")
+    
+    choice = input("請選擇功能（1-3）：")
+    
+    if choice == "3":
+        print("謝謝使用，再見！")
+        break
+    elif choice == "1":
+        a = float(input("請輸入第一個數字："))
+        b = float(input("請輸入第二個數字："))
+        print(f"結果：{a} + {b} = {a + b}")
+    elif choice == "2":
+        a = float(input("請輸入第一個數字："))
+        b = float(input("請輸入第二個數字："))
+        print(f"結果：{a} - {b} = {a - b}")
+    else:
+        print("無效的選擇，請重新選
