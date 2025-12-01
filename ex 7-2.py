6b479d3465aeabeb44ec6ad252baa4687dc2d797
christina1030915-cell <christina1030915@gmail.com>
2025-12-01 19:42:02 +0800

    Create ex 7-2.py

---
+++ ex 7-2.py
@@ -0,0 +1,18 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+print("印出 0 到 4：")
+for i in range(5):
+    print(i)
+
+print("\n印出 1 到 10：")
+for i in range(1, 11):
+    print(i, end=" ")
+print("\n\n印出 2 到 20 的偶數：")
+for i in range(2, 21, 2):
+    print(i, end=" ")
