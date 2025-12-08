2cd8e7ff93f8c1f30461c7e05960b6873744ad3f
christina1030915-cell <christina1030915@gmail.com>
2025-12-08 19:30:57 +0800

    Create ex 8-1.py

---
+++ ex 8-1.py
@@ -0,0 +1,13 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+def greet () 
+     """簡單的問候函數"""
+     print("你好 ! 歡迎學習Python ! ")
+# 呼叫函數
+def greet
