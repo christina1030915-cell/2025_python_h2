e97abc0a0ee18278ee4bfa89deee1c095562d723
christina1030915-cell <christina1030915@gmail.com>
2025-12-22 19:16:05 +0800

    Create ex 10-1.py

---
+++ ex 10-1.py
@@ -0,0 +1,22 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
+
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+
+fruits[2] = "grape"
+
+print(fruits[2])
+print(fruits)
+
+fruits.append("hello")
+print(fruits)
