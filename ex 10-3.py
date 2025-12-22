db037ec872f496de86f0f6d6e523e830dc128151
christina1030915-cell <christina1030915@gmail.com>
2025-12-22 19:44:40 +0800

    Create ex 10-3.py

---
+++ ex 10-3.py
@@ -0,0 +1,23 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+person = ("Tom", 20)
+print(person)
+
+#print(person[0])
+#print(person[1])
+
+#person[0] = "jerry"
+#person[1] = 10
+
+for p in person:
+    print(p)
+
+person.append("jerry")
+
+print(person)
